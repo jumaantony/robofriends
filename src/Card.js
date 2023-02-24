a9572616f2f75dc4b3e371485d479f581ff44237
Juma Antony <jumaantony840@gmail.com>
2023-02-24 16:48:45 +0300

    created robofriends card

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div class="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow5">
+            <img src={'https://robohash.org/${props.id}'} alt="robo_img"></img>
+            <div>
+                <h2> {props.name} </h2>
+                <p> {props.email} </p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    image: 'https://robohash.org/dhddkkw?100x100'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    image: 'https://robohash.org/dhdddfdsdfkkw?100x100'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    image: 'https://robohash.org/dhdd558kkw?100x100'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    image: 'https://robohash.org/dhdd545kkw?100x100'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    image: 'https://robohash.org/dhdddfsdw?100x100'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+    image: 'https://robohash.org/dhddkoioeikkw?100x100'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+    image: 'https://robohash.org/dhdkeiejdkkw?100x100'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
