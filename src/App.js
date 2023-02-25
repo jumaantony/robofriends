c12475c315ae5443663fe8c1d78214e6c4de6a28
Juma Antony <jumaantony840@gmail.com>
2023-02-25 10:31:10 +0300

    created a search field

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-import { render } from '@testing-library/react';
-import React from 'react';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-// function based components
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-//classbased components
-class App extends React.Component {
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
   render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
 
-        <p className='App-intro'>
-          Hello World
-        </p>
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
 
+    return(
+      <div className='tc'>
+        <h1> ROBOFRIENDS</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
       </div>
+      
     );
   }
-}
+} 
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = (props) => {
     return (
-        <div class="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow5">
+        <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow5">
             <img src={props.image} alt="robo_img"></img>
             <div>
                 <h2> {props.name} </h2>
