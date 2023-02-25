79818aec27b369ad1c3a394c4dcffa78bfe53d58
Juma Antony <jumaantony840@gmail.com>
2023-02-25 12:06:12 +0300

    tweaked image src

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow5">
-            <img src={props.image} alt="robo_img"></img>
+            <img alt='robot' src={`https://robohash.org/${props.id}?size=200x200`} />
             <div>
                 <h2> {props.name} </h2>
                 <p> {props.email} </p>
